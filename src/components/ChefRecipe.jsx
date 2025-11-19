6c336e1d3133d6b5b475c943bdb929037a11c8df
jandortiz <jefferson.garciaor@gmail.com>
2025-11-19 11:55:00 +0100

    style: delete hard-coded h2 tag.

--- src/components/ChefRecipe.jsx
+++ src/components/ChefRecipe.jsx
@@ -4,7 +4,6 @@ import ReactMarkdown from 'react-markdown'
 export default function ChefRecipe(props) {
 	return (
 		<section className="suggested-recipe-section">
-			{/* <h2>Jeff-Chef recomienda:</h2> */}
 			<article className="suggested-recipe-container" aria-live="polite">
 				<ReactMarkdown>
 					{props.response}
