1831926e971754aa00e35ed0fa4a8bbb2e15a9b6
jandortiz <jefferson.garciaor@gmail.com>
2025-11-21 09:22:55 +0100

    refactor: update website icon.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,13 @@
-import linkedinLogo from "../assets/lindedin.png"
+import linkedinLogo from "../assets/linkedin.svg"
 import githubLogo from "../assets/github-mark.png"
-import instagramLogo from "../assets/instagram-mark.png"
+import websiteIcon from "../assets/website-icon.svg"
 
 export default function Footer() {
 	return (
 		<div className="footer-container">
 			<a href="https://www.linkedin.com/in/jandortiz/" target="_blank"><img src={linkedinLogo} alt="Linkedin logo"></img></a>
 			<a href="https://github.com/jandortiz" target="_blank"><img src={githubLogo} alt="GitHub logo"></img></a>
-			<a href="https://www.instagram.com/jand_ortiz/" target="_blank"><img src={instagramLogo} alt="Instagram logo"></img></a>
+			<a href="http://jandortiz.com" target="_blank"><img src={websiteIcon} alt="Website logo"></img></a>
 		</div>
 	)
 }
