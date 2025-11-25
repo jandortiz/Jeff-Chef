87d73cb5c4c5fb558c66e4b2e8f40c2fe7f26d5c
jandortiz <jefferson.garciaor@gmail.com>
2025-11-25 18:03:28 +0100

    feat: update body.

--- src/utils/aiCalling.js
+++ src/utils/aiCalling.js
@@ -2,7 +2,7 @@ export async function getRecipeFromOpenAI(ingredientsArr){
 	const response = await fetch(import.meta.env.VITE_BACKEND_URL, {
 		method: "POST",
 		headers: {"Content-Type": "application/json"},
-		body: JSON.stringify({ingredientsArr})
+		body: JSON.stringify({ ingredients: ingredientsArr }) 
 	})
 
 	if (!response.ok) {
